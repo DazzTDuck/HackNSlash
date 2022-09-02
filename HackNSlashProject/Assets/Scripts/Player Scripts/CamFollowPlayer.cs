72e42cef90ea0ea9110cc67819c432f4ae73eb26
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-09-02 14:46:09 +0200

    player movement & looking

---
+++ HackNSlashProject/Assets/Scripts/Player Scripts/CamFollowPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float followspeed;
+    PlayerMovement pl;
+
+    private void Start()
+    {
+        pl = player.GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        Vector3 move = Vector3.Lerp(transform.position, player.position, followspeed * Time.deltaTime);
+        transform.position = move;
+        //Vector3 look = Vector3.Lerp(transform.rotation.eulerAngles, player.rotation.eulerAngles, followspeed * Time.deltaTime);
+        Quaternion look = Quaternion.Slerp(transform.rotation, player.rotation, followspeed * Time.deltaTime);
+        transform.rotation = look;
+
+    }
+}

707fd593aa5058521e7c301e3b00348d1e1759ef
frzDazzT <yerio2003@gmail.com>
2022-09-20 13:28:20 +0200

    Health linked to Healthbars

--- HackNSlashProject/Assets/Scripts/CharacterHealth.cs
+++ HackNSlashProject/Assets/Scripts/CharacterHealth.cs
@@ -1,20 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+
 
 public class CharacterHealth : MonoBehaviour
 {
     public int maxHP;
     public int currentHP;
     public int characterId;
+
     private void Start()
     {
         currentHP = maxHP;
     }
     public void TakeDamage(int damageToDo)
     {
-        EventsManager.instance.OnTakeDamage(characterId, damageToDo);
-
         if (currentHP - damageToDo <= 0)
         {
             //death
@@ -23,10 +26,11 @@ public class CharacterHealth : MonoBehaviour
         }
         else
             currentHP -= damageToDo;
+
+        EventsManager.instance.InvokeOnDamageEvent(currentHP, 0, maxHP, gameObject);
     }
-    public virtual void Death()
+    protected virtual void Death()
     {
-        EventsManager.instance.OnDeath(characterId);
         Destroy(gameObject);
     }
 }

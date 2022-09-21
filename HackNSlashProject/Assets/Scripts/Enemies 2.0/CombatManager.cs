28b7011f3ad0e49f02e9d4e407405e392388c72d
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-09-21 16:54:27 +0200

    Combat manager WIP

--- HackNSlashProject/Assets/Scripts/Enemies 2.0/CombatManager.cs
+++ HackNSlashProject/Assets/Scripts/Enemies 2.0/CombatManager.cs
@@ -41,11 +41,97 @@ public class CombatManager : MonoBehaviour
         }
     }
 
-    public void FallBack(EnemyActor actor)
+    public bool CheckToFallBack(EnemyActor actor)
     {
         if (backupEnemies.Count != 0)
         {
+            int p = actor.priority;
+            int s = actor.occupySpace;
 
+            for (int i = 0; i < backupEnemies.Count; i++)
+            {
+                if (backupEnemies[i].occupySpace <= s && backupEnemies[i].priority <= p && !backupEnemies[i].backedOff)
+                {
+                    FallingBack(actor, i);
+                    return true;
+                }
+            }
+            for (int i = 0; i < backupEnemies.Count; i++)
+            {
+                if (backupEnemies[i].occupySpace <= s && backupEnemies[i].priority <= p)
+                {
+                    FallingBack(actor, i);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    void FallingBack(EnemyActor actor, int replacement)
+    {
+        EnemyActor replacementActor = backupEnemies[replacement];
+        backupEnemies.RemoveAt(replacement);
+        for (int i = 0; i < engagedEnemies.Count; i++)
+        {
+            if (engagedEnemies[i] == actor)
+            {
+                engagedEnemies.RemoveAt(i);
+            }
+        }
+        engagedEnemies.Add(replacementActor);
+        replacementActor.state = Enemystates.Engaged;
+        backupEnemies.Add(actor);
+        actor.state = Enemystates.BackUp;
+    }
+
+    public void ResetEnemies()
+    {
+        while (engagedEnemies.Count > 0)
+        {
+            engagedEnemies[0].ReturnToPatrol();
+            engagedEnemies[0].GetComponent<CharacterHealth>().ReturnToMaxHP();
+            engagedEnemies.RemoveAt(0);
+        }
+        while (backupEnemies.Count > 0)
+        {
+            backupEnemies[0].ReturnToPatrol();
+            backupEnemies[0].GetComponent<CharacterHealth>().ReturnToMaxHP();
+            backupEnemies.RemoveAt(0);
+        }
+        while (rangedEnemies.Count > 0)
+        {
+            rangedEnemies[0].ReturnToPatrol();
+            rangedEnemies[0].GetComponent<CharacterHealth>().ReturnToMaxHP();
+            rangedEnemies.RemoveAt(0);
+        }
+        player.GetComponent<CharacterHealth>().ReturnToMaxHP();
+    }
+
+    public void RemoveFromCombat(EnemyActor actor)
+    {
+        if (actor.enemyType == EnemyType.Ranged)
+        {
+            for (int i = 0; i < rangedEnemies.Count; i++)
+            {
+                if (rangedEnemies[i] == actor)
+                    rangedEnemies.RemoveAt(i);
+            }
+        }
+        else if (actor.state == Enemystates.Attacking || actor.state == Enemystates.Engaged)
+        {
+            for (int i = 0; i < engagedEnemies.Count; i++)
+            {
+                if (engagedEnemies[i] == actor)
+                    engagedEnemies.RemoveAt(i);
+            }
+        }
+        else if (actor.state == Enemystates.BackUp)
+        {
+            for (int i = 0; i < backupEnemies.Count; i++)
+            {
+                if (backupEnemies[i] == actor)
+                    backupEnemies.RemoveAt(i);
+            }
         }
     }
 }
--- HackNSlashProject/Assets/Scripts/Enemies 2.0/EnemyActor.cs
+++ HackNSlashProject/Assets/Scripts/Enemies 2.0/EnemyActor.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class EnemyActor : MonoBehaviour
 {
+    [Header("General")]
     public CombatManager manager;
     public EnemyType enemyType;
     public Enemystates state;
     public int priority = 1;
     public int occupySpace = 1;
+    [Header("Line of sight")]
     public bool playerInSight;
     public float visionRadius;
     public float visionConeWidth;
     public LayerMask layerMask;
+    [Header("Backing off")]
+    public int hpThreshold;
+    public int attackThreshold;
+    int currentAttacks;
+    public bool backedOff;
+
+    private void Start()
+    {
+        EventsManager.instance.OnBarUpdateEvent += CheckToBackoff;
+    }
+    void CheckToBackoff(object sender = null, OnBarArgs e = null)
+    {
+        if (e?.currentAmount <= 0)
+        {
+            manager.RemoveFromCombat(this);
+        }
+        else if (state == Enemystates.Partoling)
+        {
+            manager.EnemyJoinsFight(this);
+        }
+        else if (state == Enemystates.Engaged)
+        {
+            bool backingOff = false;
+            if (e != null && (GameObject)sender == gameObject && !backedOff)
+            {
+                if (e.currentAmount <= hpThreshold)
+                {
+                    backingOff = manager.CheckToFallBack(this);
+                }
+            }
+            else if (currentAttacks >= attackThreshold)
+            {
+                backingOff = manager.CheckToFallBack(this);
+                if (backingOff)
+                    currentAttacks = 0;
+            }
+            if (backingOff)
+            {
+                backedOff = true;
+            }
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -31,6 +75,10 @@ public class EnemyActor : MonoBehaviour
 
         }
         playerInSight = false;
+
+    }
+    public void ReturnToPatrol()
+    {
 
     }
 }

6c40ca0f78d965c7be97e2fcc26357d583f6d2a8
frzDazzT <yerio2003@gmail.com>
2022-10-05 15:36:41 +0200

    SwordVisuals + Sounds

--- HackNSlashProject/Assets/Scripts/Sounds/AudioManager.cs
+++ HackNSlashProject/Assets/Scripts/Sounds/AudioManager.cs
@@ -67,7 +67,6 @@ public class AudioManager : MonoBehaviour
             if (s.playRandomClip)
             {
                 int randomIndex = Random.Range(0, s.clips.Length);
-                Debug.Log(randomIndex);
                 s.source.clip = s.clips[randomIndex];
             }
             s.source.Play();
